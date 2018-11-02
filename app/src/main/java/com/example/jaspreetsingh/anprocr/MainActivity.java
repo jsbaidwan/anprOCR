3984aad435f82ed88f98496f8411dcbc69ea0d27
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-11-02 13:07:39 +0530

    Start or restart camera source and TextRecognizer to find text

--- app/src/main/java/com/example/jaspreetsingh/anprocr/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/anprocr/MainActivity.java
@@ -3,11 +3,29 @@ package com.example.jaspreetsingh.anprocr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.vision.text.TextRecognizer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+        /**
+         * Starts or restarts the camera source, if it exists.  If the camera source doesn't exist yet
+         * (e.g., because onResume was called before the camera source was created), this will be called
+         * again when the camera source is created.
+         */
+    private void startCameraSource() {
+
+        // A text recognizer is created to find text.  An associated multi-processor instance
+        // is set to receive the text recognition results, track the text, and maintain
+        // graphics for each text block on screen.  The factory is used by the multi-processor to
+        // create a separate tracker instance for each text block.
+        final TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
     }
 }
